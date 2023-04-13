2e78262de517ea86f0e64990d902f3af54cf5fc1
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-13 23:00:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ function rotateLine() {
+      var line = document.getElementById("line");
+      var rotationAngle = 0;
+      setInterval(function() {
+        rotationAngle += 5;
+        line.style.transform = "rotate(" + rotationAngle + "deg)";
+      }, 50);
+    }
+    rotateLine();
